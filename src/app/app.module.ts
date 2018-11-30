11bbbf2a64c04d0bd0ab5e4ef33714742c6004b5
Севостьянов Олег Константинович <oleg.sevostyanov@sms-a.ru>
2018-11-30 10:36:43 +0400

    Add personal info and project list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ProjectListComponent } from './project-list/project-list.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +7,9 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInfoComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule
