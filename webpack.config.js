32769474cb6cfd144d86c97aec2083d9f44a74e5
Oleg Sevostyanov <oleg.sev@gmail.com>
2018-12-04 21:14:15 +0400

    Configure webpack build. Step 1.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,51 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const distPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
+    mode: 'production',
+    devtool: '',
     entry: './src/main.ts',
     module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-        }
-      ]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: [/\.spec\.ts$/, /node_modules/, nodeModulesPath],
+            },
+        ],
     },
     resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ]
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    }    
+        filename: '[name].js',
+        path: distPath
+    },
+    plugins: [
+        new CleanWebpackPlugin([distPath]),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+        minimizer: [
+            new UglifyJsPlugin({
+                extractComments: true
+            }),
+        ],
+    },
 };
