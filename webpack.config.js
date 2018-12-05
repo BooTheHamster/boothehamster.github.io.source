6b25d29854332678e7151da244bf2b149aeb1167
Севостьянов Олег Константинович <oleg.sevostyanov@sms-a.ru>
2018-12-05 10:28:02 +0400

    Configure webpack build. Typescripts and styles.

--- webpack.config.js
+++ webpack.config.js
@@ -5,32 +5,62 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const distPath = path.resolve(__dirname, 'dist');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
 
 module.exports = {
-    mode: 'production',
-    devtool: '',
-    entry: './src/main.ts',
+    mode: 'development',
+    entry: {
+        polyfills: './src/polyfills.ts',
+        app: './src/main.ts',
+        styles: './src/styles.less'
+    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: [/\.spec\.ts$/, /node_modules/, nodeModulesPath],
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: {
+                            configFileName: tsconfigPath,
+                        },
+                    },
+                    'angular2-template-loader',
+                    'angular2-router-loader',
+                ],
+                exclude: [/\.(spec|e2e)\.ts$/, /node_modules/, nodeModulesPath],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'less-loader'],
+                }),
+                exclude: [/node_modules/, nodeModulesPath],
             },
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.tsx', '.ts', '.js', '.less'],
     },
     output: {
-        filename: '[name].js',
-        path: distPath
+        publicPath: '/',
+        filename: '[name].[hash].js',
+        path: distPath,
     },
     plugins: [
         new CleanWebpackPlugin([distPath]),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+        new HtmlWebpackPlugin(),
+        new ExtractTextPlugin('styles.css'),
     ],
     optimization: {
         splitChunks: {
@@ -44,7 +74,7 @@ module.exports = {
         },
         minimizer: [
             new UglifyJsPlugin({
-                extractComments: true
+                extractComments: true,
             }),
         ],
     },
