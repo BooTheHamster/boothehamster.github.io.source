152a7ca6a06fec84b17f9c3d6347377456b71a74
Oleg Sevostyanov <oleg.sev@gmail.com>
2018-12-06 21:51:44 +0400

    Trying write kill devserver script

--- kill-dev-server.js
+++ kill-dev-server.js
@@ -1,5 +1,34 @@
-const psList = require('ps-list');
- 
-(async () => {
-    let processes = await psList();
-})();
+const ps = require('ps-node');
+
+const getProcesses = async () => {
+    let result = [];
+
+    await ps.lookup({}, (error, resultList) => {
+        if (error) {
+            throw new Error(error);
+        }
+
+        resultList.forEach(process => {
+            if (!process) {
+                return;
+            }
+
+            let command = process.command;
+
+            if (process.arguments) {
+                process.arguments.forEach(
+                    arg => (command += ` ${arg.toString()}`)
+                );
+            }
+
+            result.push({ pid: process.pid, name: command });
+
+            return result;
+        });
+    });
+
+    return result;
+};
+
+var r = getProcesses();
+var t = 1;
